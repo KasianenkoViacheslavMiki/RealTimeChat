ec0c06588ab3599fc88a4d2b8e42c8d1199abe90
MikiDJ <slava.avals.2001@gmail.com>
2022-09-09 13:23:23 +0300

    Created and added ViewComponent in shared blank _Layuot.

---
+++ Reenbit.TestTask.RealTimeChat/ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reenbit.TestTask.RealTimeChat.Models;
+
+namespace Reenbit.TestTask.RealTimeChat.ViewComponents
+{
+    public class RoomViewComponent: ViewComponent
+    {
+        private readonly ChatDBContext chatDBContext;
+        public RoomViewComponent(ChatDBContext _chatDBContext)
+        {
+            chatDBContext = _chatDBContext;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            var room = chatDBContext.Rooms.ToList();
+            return View(room);
+        }
+
+    }
+}
